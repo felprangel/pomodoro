8a65d291c6b8f1e1656f619d6930ce08d68f5064
felprangel <feliperangel897@gmail.com>
2024-12-17 21:30:25 -0300

    adiciona estilização para o botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,21 @@
 import { ButtonHTMLAttributes } from "react";
+import styled from "styled-components";
 
 export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props}>{props.children}</button>;
+  return <PomodoroButton {...props}>{props.children}</PomodoroButton>;
 }
+
+const PomodoroButton = styled.button`
+  background-color: var(--white);
+  color: var(--pomodoro);
+  padding: 1rem 3rem;
+  font-weight: 700;
+  font-size: 1.5rem;
+  border: none;
+  border-radius: 0.5rem;
+
+  &:hover {
+    filter: brightness(0.9);
+    cursor: pointer;
+  }
+`;
