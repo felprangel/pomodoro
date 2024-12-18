4d5d1fbed247d2cabe26fd36ce4a61f256752259
felprangel <feliperangel897@gmail.com>
2024-12-17 21:18:50 -0300

    ajusta posicionamento do container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,4 +26,7 @@ const TimerContainer = styled.div`
   height: 45vh;
   background-color: #c15c5c;
   border-radius: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
