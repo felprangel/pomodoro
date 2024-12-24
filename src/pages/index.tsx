e6315f8d5b220589a2dac0b8d4c97d1c70b45266
felprangel <feliperangel897@gmail.com>
2024-12-23 22:44:01 -0300

    adiciona alteração de cor condicionalmente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
 import { Header } from "@/components/Header";
 import PageLayout from "@/components/PageLayout";
 import { Timer } from "@/components/Timer";
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Home() {
+  const [isWorking, setIsWorking] = useState<boolean>(true);
+
   return (
     <>
       <Header />
-      <CustomPageLayout>
-        <Timer />
+      <CustomPageLayout working={isWorking}>
+        <Timer changeBgColor={setIsWorking} />
       </CustomPageLayout>
     </>
   );
 }
 
-const CustomPageLayout = styled(PageLayout)`
-  background-color: var(--pomodoro);
+const CustomPageLayout = styled(PageLayout)<{ working: boolean }>`
+  background-color: ${(props) =>
+    props.working ? "var(--pomodoro)" : "var(--break-time)"};
   color: var(--white);
   min-height: 100vh;
   min-width: 100vw;
