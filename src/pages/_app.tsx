fdd73ad7351e14956993ed3ef74f186085daddda
felprangel <feliperangel897@gmail.com>
2024-12-29 19:42:40 -0300

    adiciona título a página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { GlobalStyle } from "@/styles/globals";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Pomodoro Timer</title>
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </>
